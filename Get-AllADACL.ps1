2fa71e885bdbea13eaf78bf611405926c86be34a
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2023-05-17 17:05:14 +0200

    Create Get-AllADACL.ps1

---
+++ Get-AllADACL.ps1
@@ -0,0 +1,25 @@
+# Will output all the ACLs of a domain in four files
+
+Set-ExecutionPolicy Bypass -Force
+. .\Get-ADObjectACL.ps1 # 
+
+# Domain partition
+Get-ADObject -Filter * | Select -ExpandProperty DistinguishedName | Get-ADObjectACL -ExcludeInherited -IncludeOwner -FullOutput > acls-domain.txt
+
+# Schema partition
+Get-ADObject -Filter * -SearchBase "CN=Schema,CN=Configuration,DC=external,DC=local" | Select -ExpandProperty DistinguishedName | Get-ADObjectACL -ExcludeInherited -IncludeOwner -FullOutput > acls-schema.txt
+
+# Config partition
+Get-ADObject -Filter * -SearchBase "CN=Configuration,DC=external,DC=local" | Select -ExpandProperty DistinguishedName | Get-ADObjectACL -ExcludeInherited -IncludeOwner -FullOutput > acls-config.txt
+
+# Default sec descriptors
+$objs = Get-ADObject -Filter * -SearchBase "CN=Schema,CN=Configuration,DC=external,DC=local" -Properties defaultSecurityDescriptor 
+foreach ($obj in $objs) {
+    if ($obj.ObjectClass -eq "classSchema") {
+        $obj.defaultSecurityDescriptor >> acls-classes.txt
+        $obj.DistinguishedName >> acls-classes.txt
+        ConvertFrom-SddlString -Sddl $obj.defaultSecurityDescriptor | Select -ExpandProperty DiscretionaryAcl >> acls-classes.txt
+        "" >> acls-classes.txt
+        "" >> acls-classes.txt
+    }
+}
