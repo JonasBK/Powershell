4f78565feb7646b573d575020eed16bcbd989618
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2024-02-23 11:32:30 +0100

    Update Get-X509IssuerSerialNumberFormat.ps1

--- Get-X509IssuerSerialNumberFormat.ps1
+++ Get-X509IssuerSerialNumberFormat.ps1
@@ -1,21 +1,21 @@
 function Get-X509IssuerSerialNumberFormat {
   <# Example usage
-  $issuerDistinguishedName = "CN=CONTOSO-DC-CA,DC=contoso,DC=com"
-  $serialNumber = "2B0000000011AC0000000012"
-  Get-X509IssuerSerialNumberFormat -serialNumber $serialNumber -issuerDistinguishedName $issuerDistinguishedName
+  $IssuerDistinguishedName = "CN=CONTOSO-DC-CA,DC=contoso,DC=com"
+  $SerialNumber = "2B0000000011AC0000000012"
+  Get-X509IssuerSerialNumberFormat -SerialNumber $SerialNumber -IssuerDistinguishedName $IssuerDistinguishedName
   #>
   param (
-        [string]$serialNumber,
-        [string]$issuerDistinguishedName
+        [string]$SerialNumber,
+        [string]$IssuerDistinguishedName
     )
 
     # Reverse the serial number
-    $reversedSerialNumber = $serialNumber -split "(..)"
+    $reversedSerialNumber = $SerialNumber -split "(..)"
     [array]::Reverse($reversedSerialNumber)
     $reversedSerialNumber = $reversedSerialNumber -join ''
 
     # Split the issuer distinguished name into components and reverse them
-    $reversedIssuerComponents = $issuerDistinguishedName -split ','
+    $reversedIssuerComponents = $IssuerDistinguishedName -split ','
     [array]::Reverse($reversedIssuerComponents)
     $reversedIssuerComponents = $reversedIssuerComponents -join ','
 
