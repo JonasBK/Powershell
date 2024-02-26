c269ef4afaf0e338b25dd3e3223cb9fe302254e1
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2024-02-26 10:25:05 +0100

    Update Get-AltSecIDMapping.ps1

--- Get-AltSecIDMapping.ps1
+++ Get-AltSecIDMapping.ps1
@@ -1,17 +1,45 @@
 function Get-AltSecIDMapping {
   <# Example usage
-    Get-AltSecIDMapping -DistinguishedName "CN=Administrator,CN=Users,DC=dumpster,DC=fire"
+    Get-AltSecIDMapping -SearchBase "CN=Users,DC=dumpster,DC=fire"
     
     <Output:>
+    CN=Administrator,CN=Users,DC=dumpster,DC=fire
     X509:<S>DC=fire,DC=dumpster,CN=Users,CN=Andy3
     X509:<S>DC=fire,DC=dumpster,CN=Users,CN=Andy2
     X509:<S>DC=fire,DC=dumpster,CN=Users,CN=Andy
   #>
   param (
-        [string]$DistinguishedName
+          [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0
+            )]
+        [string]$SearchBase,
+        [Parameter(
+            Mandatory = $false
+            )]
+        [switch]$ExcludeStrong = $false
     )
 
-    $target = [ADSI]"LDAP://$DistinguishedName"
-    $value = $target.Properties["altSecurityIdentities"].Value
-    $value | ForEach-Object { Write-Host $_ }
+    $searcher = [adsisearcher]""
+    $searcher.SearchRoot = "LDAP://$SearchBase"
+    $searcher.Filter = "(objectClass=*)"
+    $searcher.PropertiesToLoad.AddRange(@("altSecurityIdentities", "distinguishedName"))
+
+    $results = $searcher.FindAll()
+
+    foreach ($result in $results) {
+        $altSecIdentities = $result.Properties["altSecurityIdentities"]
+        $selectedAltSecIdentities = $altSecIdentities | ? { 
+            -not $ExcludeStrong `
+            -or ($_ -notmatch '^X509:<(SKI|SHA1-PUKEY)>' `
+            -and $_ -notmatch '^X509:<I>.*<SR>')
+        }
+        if ($selectedAltSecIdentities -ne $null) {
+            Write-Host "`r"
+            Write-Host $result.Properties["distinguishedName"]
+            $selectedAltSecIdentities | ForEach-Object { Write-Host $_ }
+        }
+    }
 }
