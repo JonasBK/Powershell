099189d6c08bd63cd9fd8236a602b87c19056086
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2024-07-30 22:32:19 +0200

    Update Get-CreateRenameGroupACEs.ps1

--- Get-CreateRenameGroupACEs.ps1
+++ Get-CreateRenameGroupACEs.ps1
@@ -13,24 +13,30 @@ Function Get-CreateRenameGroupACEs {
     By default the returned ACL will include inherited ACEs. Use this switch to exclude inherited ACEs.
 
 .PARAMETER ExcludeCreate
-    Exclude create group ACEs
+    Exclude create group ACEs.
 
-.PARAMETER ExcludeCreate
-    Exclude rename ACEs
+.PARAMETER ExcludeRename
+    Exclude rename ACEs.
+
+.PARAMETER UniqueIdentityReferences
+    Only return unique IdentityReference values and their corresponding ObjectDN values.
+
+.PARAMETER ExcludeIdentityReferences
+    List of IdentityReferences to exclude from the results.
 
 .EXAMPLE
     PS> Get-CreateRenameGroupACEs -DistinguishedName "dc=hackme,dc=local"
-    Get the ACEs for a single object based on DistinguishedName
+    Get the ACEs for a single object based on DistinguishedName.
 
 .EXAMPLE
     PS> Get-ADObject -Filter * -SearchBase "dc=hackme,dc=local" | Get-CreateRenameGroupACEs
-    Get ACEs of all AD objects under domain root by piping them into Get-CreateRenameGroupACEs
+    Get ACEs of all AD objects under domain root by piping them into Get-CreateRenameGroupACEs.
 
 .LINK
     https://github.com/JonasBK/Powershell/blob/master/Get-CreateRenameGroupACEs.ps1
 
 .INPUTS
-    Supports both pipleine from Get-ADObject or a string formatted as DistinguishedName.
+    Supports both pipeline from Get-ADObject or a string formatted as DistinguishedName.
 #>
  
     [CmdletBinding()]
@@ -40,28 +46,32 @@ Function Get-CreateRenameGroupACEs {
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true,
             Position = 0
-            )]
+        )]
         [string]$DistinguishedName,
         [Parameter(
             Mandatory = $false
-            )]
+        )]
         [string]$Domain,
         [Parameter(
             Mandatory = $false
-            )]
+        )]
         [switch]$ExcludeInherited,
         [switch]$ExcludeCreate,
-        [switch]$ExcludeRename
+        [switch]$ExcludeRename,
+        [switch]$UniqueIdentityReferences,
+        [Parameter(
+            Mandatory = $false
+        )]
+        [string[]]$ExcludeIdentityReferences
     )
  
     BEGIN {
-        # Use current domain if Domian is null
+        # Use current domain if Domain is null
         if (($null -eq $Domain) -or ($Domain -eq "")) {
             $Domain = (Get-ADDomain).DNSRoot
         }
 
-        # Get AD schma GUIDs and their Names
-        ### Currently all GUIDs are gathered into a hashtable which is later queried per object. Could be changed so that a lookup is performer per object.
+        # Get AD schema GUIDs and their Names
         $ADRootDSE = Get-ADRootDSE -Server $Domain
         $schemaNamingContext = $ADRootDSE.schemaNamingContext
         $configurationNamingContext = $ADRootDSE.configurationNamingContext
@@ -75,7 +85,7 @@ Function Get-CreateRenameGroupACEs {
             }
         }
 
-        # Create PS drive for the select AD domain and jump to that drive
+        # Create PS drive for the selected AD domain and jump to that drive
         $oldLocation = Get-Location
         $adDriveName = "ADDrive-$($Domain.Replace('.','-'))"
         if ($oldLocation.Path -ne ($adDriveName + ":\")) {
@@ -83,10 +93,12 @@ Function Get-CreateRenameGroupACEs {
             $null = New-PSDrive -Name $adDriveName -PSProvider ActiveDirectory -Server $Domain -Root "//RootDSE/"
             Set-Location "$($adDriveName):"
         }
+
+        $resACEs = @()
+        $identityReferences = @{}
     }
  
     PROCESS {
-        $resACEs = @()
         $acl = Get-Acl -LiteralPath "Microsoft.ActiveDirectory.Management.dll\ActiveDirectory:://RootDSE/$DistinguishedName"
         
         if ($ExcludeInherited) {
@@ -98,15 +110,19 @@ Function Get-CreateRenameGroupACEs {
         }
   
         foreach ($permission in $objPermissions) {
-                $permission | Add-Member -NotePropertyName ObjectDN -NotePropertyValue $DistinguishedName
-                $permission | Add-Member -NotePropertyName InheritedObjectTypeName -NotePropertyValue $schemaIDGUID[[System.GUID]$permission.inheritedObjectType]
-                $permission | Add-Member -NotePropertyName ObjectTypeName -NotePropertyValue $schemaIDGUID[[System.GUID]$permission.ObjectType]
+            if ($ExcludeIdentityReferences -and $ExcludeIdentityReferences -contains $permission.IdentityReference) {
+                continue
+            }
+
+            $permission | Add-Member -NotePropertyName ObjectDN -NotePropertyValue $DistinguishedName -Force
+            $permission | Add-Member -NotePropertyName InheritedObjectTypeName -NotePropertyValue $schemaIDGUID[[System.GUID]$permission.inheritedObjectType] -Force
+            $permission | Add-Member -NotePropertyName ObjectTypeName -NotePropertyValue $schemaIDGUID[[System.GUID]$permission.ObjectType] -Force
 
             if (-not $ExcludeRename) {
                 # Check if rename permission
-                if (($Permission.ObjectTypeName -eq "RDN" `
-                        -or $Permission.ObjectTypeName -eq "Public-Information" `
-                        -or $Permission.ObjectType -eq "00000000-0000-0000-0000-000000000000") `
+                if (($permission.ObjectTypeName -eq "RDN" `
+                        -or $permission.ObjectTypeName -eq "Public-Information" `
+                        -or $permission.ObjectType -eq "00000000-0000-0000-0000-000000000000") `
                     -and $permission.AccessControlType -eq "Allow" `
                     -and ($permission.ActiveDirectoryRights -band [System.DirectoryServices.ActiveDirectoryRights]::WriteProperty) `
                     -and ($permission.InheritanceType -eq [System.DirectoryServices.ActiveDirectorySecurityInheritance]::None `
@@ -117,7 +133,7 @@ Function Get-CreateRenameGroupACEs {
 
             if (-not $ExcludeCreate) {
                 # Check if create permission
-                if (($permission.ObjectTypeName -eq "Group" -or $Permission.ObjectType -eq "00000000-0000-0000-0000-000000000000") `
+                if (($permission.ObjectTypeName -eq "Group" -or $permission.ObjectType -eq "00000000-0000-0000-0000-000000000000") `
                     -and $permission.AccessControlType -eq "Allow" `
                     -and ($permission.ActiveDirectoryRights -band [System.DirectoryServices.ActiveDirectoryRights]::CreateChild) `
                     -and ($permission.InheritanceType -eq [System.DirectoryServices.ActiveDirectorySecurityInheritance]::None `
@@ -127,8 +143,17 @@ Function Get-CreateRenameGroupACEs {
                 }
             }
         }
-    
-        Return $resACEs
+
+        if ($UniqueIdentityReferences) {
+            foreach ($ace in $resACEs) {
+                if (-not $identityReferences.ContainsKey($ace.IdentityReference)) {
+                    $identityReferences[$ace.IdentityReference] = @()
+                }
+                if ($ace.ObjectDN -notin $identityReferences[$ace.IdentityReference]) {
+                    $identityReferences[$ace.IdentityReference] += $ace.ObjectDN
+                }
+            }
+        }
     }
 
     END {
@@ -141,6 +166,17 @@ Function Get-CreateRenameGroupACEs {
             catch [System.Management.Automation.DriveNotFoundException] {
                 # Stay at current location if our old has been removed
             }
+        }
+
+        if ($UniqueIdentityReferences) {
+            $identityReferences.GetEnumerator() | ForEach-Object {
+                [PSCustomObject]@{
+                    IdentityReference = $_.Key
+                    ObjectDNs         = $_.Value -join ", "
+                }
+            }
+        } else {
+            $resACEs
         }
     }
 }
