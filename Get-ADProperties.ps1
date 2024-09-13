fb85b11e4c2a9312914374a37b9ea7da90f36cde
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2024-09-13 08:48:06 +0200

    Update Get-ADProperties.ps1

--- Get-ADProperties.ps1
+++ Get-ADProperties.ps1
@@ -1,10 +1,13 @@
 $searcher = [adsisearcher]""
 $searcher.SearchRoot = "LDAP://DC=dumpster,DC=fire"
 $searcher.Filter = "(objectClass=*)"
-$searcher.PropertiesToLoad.AddRange(@("lockoutDuration", "distinguishedName"))
+$searcher.PropertiesToLoad.AddRange(@("distinguishedName", "minPwdAge"))
 
 $results = $searcher.FindAll()
 
 foreach ($result in $results) {
-    Write-Host "$($result.Properties["distinguishedName"]) $($result.Properties["lockoutDuration"])"
+    if ($result.Properties["minPwdAge"] -ne "") {
+        $result.Properties["distinguishedName"]
+        $result.Properties["minPwdAge"]
+    }
 }
