026cd882a22080093c9012f4a2c22ee3b197db3b
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2025-02-06 11:31:18 +0100

    Update and rename Check-DeletedObject.ps1 to Get-DeletedObjects.ps1

--- Get-DeletedObjects.ps1
+++ Get-DeletedObjects.ps1
@@ -1,5 +1,15 @@
-# You can check if a SID is a deleted AD object if you run this query here on a DC as Domain Admin:
+# Raw LDAP enum:
+$root = New-Object DirectoryServices.DirectoryEntry("LDAP://DC=BASTION,DC=LOCAL")
+$searcher = New-Object System.DirectoryServices.DirectorySearcher
+$searcher.SearchRoot = $root
+$searcher.Filter = "(&(isDeleted=TRUE))"
+$searcher.SearchScope = "Subtree"
+$searcher.Tombstone = $true  # Ensures search includes tombstoned objects
+$searcher.FindAll()
 
+# Requires List permission on the Deleted Objects container
+
+# You can check if a SID is a deleted AD object if you run this query:
 Get-ADObject -IncludeDeletedObjects -Filter 'ObjectSID -eq "S-1-5-21-2697957641-2271029196-387917394-1103"' -Properties ObjectSID
 
 # If the SID represents a recently deleted object (and recycle bin is enabled), it will have Deleted = True in the output. If the SID does not belong to any object (incl. recently deleted), the cmdlet will output nothing. If the SID represents an existing non-deleted object, it will be shown in the output as well with the Deleted property set to nothing.
